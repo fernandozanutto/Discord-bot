91b03a369f2a743144c1fd48010816a48fcdef86
Fernando Zanutto <ferzanutto1999@gmail.com>
2019-02-20 17:01:21 -0300

    Update bot.py
    Bot envia mensagem com o tempo em segundos que ficou em algum jogo

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 import discord
 import asyncio
+from time import time
 
 token_file = open('token.txt', 'r')
 token = token_file.readline()[:-1]
-client = discord.Client()
+client = discord.Client(status="Sugooooi")
 
 @client.event
 async def on_ready():
@@ -15,15 +16,30 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.startswith('!test'):
-        counter = 0
-        tmp = await message.channel.send('Calculating messages...')
-        async for log in message.channel.history():
-            if log.author == message.author:
-                counter += 1
-                
-        await tmp.edit(content = "You have {} messages.".format(counter))
-    elif message.content.startswith('!sleep'):
-        await asyncio.sleep(5)
-        await message.channel.send('Done sleeping')
+
+        tmp = await message.channel.send('Yeee, estou funcionando')
+
+
+@client.event
+async def on_member_update(before, after):
+
+    now = time()
+
+    for activity in before.activities:
+        if activity.type == discord.ActivityType.playing:
+
+            diferenca = now - activity.timestamps['start']/1000
+
+            #if before.dm_channel is None:
+            #    await before.create_dm()
+
+            mensagem = '{} jogou {} por {} segundos'.format(before.mention, activity.name, diferenca)
+            #await before.dm_channel.send(mensagem)
+            await before.guild.text_channels[0].send(mensagem)
+
 
 client.run(token)
+
+
+#keep track of most played game
+#keep track of who played most time
